f91eaabbea7604ccd0fa6838ead169c6b74914ad
LnX <luan38.eric@gmail.com>
2022-02-14 10:11:13 -0300

    finalizando projeto

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
 import { UserStorage } from "./UserContext";
+import Photo from './Components/Photo/Photo';
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 function App() {
 
@@ -13,13 +18,19 @@ function App() {
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/login/*" element={<Login />}/>
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="login/*" element={<Login />}/>
+              <ProtectedRoute path="conta/*" element={<User/>}/>
+              <Route path="foto/:id" element={<Photo />}/>
+              <Route path="perfil/:user" element={<UserProfile />}/>
+              <Route path="*" element={<NotFound />}/>
+            </Routes>
+          </main>
           <Footer/>
         </UserStorage>
-     </BrowserRouter>
+     </BrowserRouter> 
     </div>
   );
 }
