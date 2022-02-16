3c2f5036df5975020da33c6b7c10c063b6341c89
LnX <luan38.eric@gmail.com>
2022-02-16 16:57:11 -0300

    atualização no head

--- src/Components/Helper/Head.js
+++ src/Components/Helper/Head.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Head = (props) => {
+  React.useEffect(() => {
+    document.title = props.title + ' | Dogs';
+    document
+      .querySelector("meta[name='description']")
+      .setAttribute('content', props.description || '');
+  }, [props]);
 
-    React.useEffect(() => {
-        document.title = props.title + ' | Dogs';
-        document.querySelector("meta[name='description']")
-        .setAttribute('content', props.description || '')
-    }, [props]);
+  return <></>;
+};
 
-  return (
-    <>Head</>
-  )
-}
-
-export default Head
+export default Head;
